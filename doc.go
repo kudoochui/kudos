63df128f0ea72c0cf41cae5c1c99c7183545c4f9
Chenghui <chenghui@qq.com>
2020-03-10 19:14:44 +0800

    add doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Kudos is a simple, high-performance, easy to expand and easy to deploy distributed game service framework
+based on microservice architecture, It is based on RPC of rpcx, supports pomelo communication protocol and
+can be easily applied to game development.
+ */
+package kudos
